115236923f49e1840905010f2f6359ee8b8c1ea0
quockhanhtran220407-maker <quockhanhtran220407@gmail.com>
2026-02-04 17:19:23 +0700

    Update Book Manangement.py

--- Book Manangement.py
+++ Book Manangement.py
@@ -3,12 +3,13 @@ class Book:
         self.name = name
         self.id = id
         self.author = author
-    def book(self):
+    def information(self):
         return f"Book's name : {self.name}, ID : {self.id}, Author : {self.author}"
 
 class BookManangement:
     def __init__(self):
         self.library = []
+        
     def addBook(self):
         name = input("Enter Book's name : ")
         id = input("Enter Book's ID : ")
@@ -19,20 +20,25 @@ class BookManangement:
     def displayBook(self):
         for e in self.library:
             print(e.name, " | ", e.id ," | ", e.author)
-            print()
+            
     def display_booklist(self):
         pass
+        
     def search_for_book(self):
         InputID = input('Nhập ID sách: ')
         for n in self.library:
             if n[1] == InputID:
                 print(n.name, " | ", n.id ," | ", n.author)
-
-        pass
+                
     def edit_book_information(self):
         pass
+        
     def delete_book(self):
-        pass
+        removed_ID = input("Enter the removed book's ID : ")
+        for book in self.library:
+            if book.id == removed_ID:
+                self.library.remove(book)
+        
     def borrow_book(self):
         pass
     def return_book(self):
@@ -49,3 +55,4 @@ library=BookManangement()
 library.library=[Book("book1", "ID1", "author1"),Book("book2", "ID2", "author2"),Book("book3", "ID3", "author3")]
 
 library.displayBook()
+
