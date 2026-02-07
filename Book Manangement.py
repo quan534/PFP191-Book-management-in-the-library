81ad72ddd3156df8ed8338119f68f58df1faa29d
huy1007kt-ctrl <huy1007kt@gmail.com>
2026-02-07 07:34:25 +0700

    Update Book Manangement.py

--- Book Manangement.py
+++ Book Manangement.py
@@ -1,10 +1,24 @@
 class Book:
-    def __init__(self, name, id, author):
+    def __init__(self, name, id, author, quantity):
         self.name = name
         self.id = id
         self.author = author
+        self.quantity = quantity
+        self.borrow_count = 0
     def information(self):
         return f"Book's name : {self.name}, ID : {self.id}, Author : {self.author}"
+
+    def is_available(self):
+        return self.quantity>0
+
+    def borrow(self):
+        if not self.is_available():
+            raise ValueError('This book is currently not available')
+        self.quantity -= 1
+        self.borrow_count += 1
+
+    def return_book(self):
+        self.quantity +=1
 
 class BookManangement:
     def __init__(self):
@@ -16,11 +30,12 @@ class BookManangement:
         author = input("Enter Book's Author : ")
         book = Book(name, id, author)
         self.library.append(book)
-    
+ #cho nay neu nhu k co sach thi co display gi ko ong   
     def displayBook(self):
         for e in self.library:
             print(e.name, " | ", e.id ," | ", e.author)
-            
+
+   
     def display_booklist(self):
         pass
         
@@ -38,8 +53,23 @@ class BookManangement:
         for book in self.library:
             if book.id == removed_ID:
                 self.library.remove(book)
-        
-    def borrow_book(self):
+
+    def find_book_by_id(self, id):
+        for book in self.library:
+            if book.id == id:
+                return book
+        return None
+    def borrow_book(self,id):
+        book = self.find_book_by_id(id)
+        if book is None:
+            print('Book not found')
+            return
+        try:
+            book.borrow()
+            print('Borrow successfully')
+            print(book)
+        except ValueError as e:
+            print(e)
         pass
     def return_book(self):
         pass
@@ -55,4 +85,5 @@ library=BookManangement()
 library.library=[Book("book1", "ID1", "author1"),Book("book2", "ID2", "author2"),Book("book3", "ID3", "author3")]
 
 library.displayBook()
+
 
