b0e3eb5b236d8a19f7199fc32d3564b2936d8703
quanminhon534 <quanminhon534@gmail.com>
2026-02-07 10:18:01 +0700

    Update Book Manangement.py

--- Book Manangement.py
+++ Book Manangement.py
@@ -22,21 +22,18 @@ class BookManangement:
         quantity = int(input("Enter the number of books : "))
         book = Book(name, id, author,quantity)
         self.library.append(book)
+        print("book added!")
 
- #cho nay neu nhu k co sach thi co display gi ko ong   
-    def displayBook(self,book_id):
-        book=self.find_book_by_id(book_id)
-        if book == None:
-            print("book not found!")
-        else:
-            book.information()
-
+ #cho nay neu nhu k co sach thi co display gi ko ong 
+# tui sửa rồi nha ô  
    
     def display_booklist(self):
+        print("Name".ljust(35), " | ", "Id".ljust(5), " | ", "Author".ljust(20), " | ", "Quantity")
+        print("-----------------------------------------------------------------------------------")
         for e in self.library:
-            print(e.name, " | ", e.id ," | ", e.author," | " , e.quantity)
+            print(e.name. ljust(35), " | ", e.id.ljust(5) ," | ", e.author.ljust(20)," | " , e.quantity)
         
-        
+                
     def search_for_book(self):
         InputID = input('Nhập ID sách: ')
         for n in self.library:
@@ -57,6 +54,7 @@ class BookManangement:
             if book.id == id:
                 return book
         return None
+    
     def borrow_book(self,id):
         book = self.find_book_by_id(id)
         if book is None:
@@ -69,18 +67,37 @@ class BookManangement:
         except ValueError as e:
             print(e)
         pass
+
     def return_book(self):
         pass
+    
     def view_borrowed_book(self):
         pass
         
-        
-
-
-
-
 library=BookManangement()
-library.library=[Book("book1", "ID1", "author1",1),Book("book2", "ID2", "author2",2),Book("book3", "ID3", "author3",3)]
+
+library.library = [
+    Book("Dế Mèn Phiêu Lưu Ký", "1", "Tô Hoài", 5),
+    Book("Tuổi Thơ Dữ Dội", "2", "Phùng Quán", 3),
+    Book("Lão Hạc", "3", "Nam Cao", 4),
+    Book("Chí Phèo", "4", "Nam Cao", 6),
+    Book("Tắt Đèn", "5", "Ngô Tất Tố", 2),
+    Book("Vợ Nhặt", "6", "Kim Lân", 3),
+    Book("Số Đỏ", "7", "Vũ Trọng Phụng", 5),
+    Book("Nhật Ký Trong Tù", "8", "Hồ Chí Minh", 4),
+    Book("Rừng Xà Nu", "9", "Nguyễn Trung Thành", 6),
+    Book("Đất Rừng Phương Nam", "10", "Đoàn Giỏi", 7),
+    Book("Mắt Biếc", "11", "Nguyễn Nhật Ánh", 8),
+    Book("Cho Tôi Xin Một Vé Đi Tuổi Thơ", "12", "Nguyễn Nhật Ánh", 6),
+    Book("Người Lái Đò Sông Đà", "13", "Nguyễn Tuân", 3),
+    Book("Chiếc Thuyền Ngoài Xa", "14", "Nguyễn Minh Châu", 4),
+    Book("Vợ Chồng A Phủ", "15", "Tô Hoài", 5),
+    Book("Hai Đứa Trẻ", "16", "Thạch Lam", 2),
+    Book("Cánh Đồng Bất Tận", "17", "Nguyễn Ngọc Tư", 4),
+    Book("Nỗi Buồn Chiến Tranh", "18", "Bảo Ninh", 3),
+    Book("Dòng Sông Ly Biệt", "19", "Nguyễn Mộng Giác", 2),
+    Book("Bến Không Chồng", "20", "Dương Hướng", 3)
+]
 
 library.display_booklist()
 
