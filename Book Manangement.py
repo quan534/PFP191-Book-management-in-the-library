9075005ad5fdfe6e0cfe5bb63bd9f9d0a4e71803
quanminhon534 <quanminhon534@gmail.com>
2026-02-07 10:42:23 +0700

    Update Book Manangement.py

--- Book Manangement.py
+++ Book Manangement.py
@@ -1,13 +1,11 @@
 class Book:
-    def __init__(self, name, id, author, quantity):
+    def __init__(self, name, id, author, quantity, category):
         self.name = name
         self.id = id
         self.author = author
         self.quantity = quantity
+        self.category=category
  
-    def information(self):
-        return f"Book's name : {self.name}, ID : {self.id}, Author : {self.author}, Quantity : {self.quantity}"
-
     def __str__(self):
         return f"Book's name : {self.name}, ID : {self.id}, Author : {self.author}"
 
@@ -20,7 +18,8 @@ class BookManangement:
         id = input("Enter Book's ID : ")
         author = input("Enter Book's Author : ")
         quantity = int(input("Enter the number of books : "))
-        book = Book(name, id, author,quantity)
+        category = input("Enter Book's category: ")
+        book = Book(name, id, author,quantity,category)
         self.library.append(book)
         print("book added!")
 
@@ -28,10 +27,10 @@ class BookManangement:
 # tui sửa rồi nha ô  
    
     def display_booklist(self):
-        print("Name".ljust(35), " | ", "Id".ljust(5), " | ", "Author".ljust(20), " | ", "Quantity")
-        print("-----------------------------------------------------------------------------------")
+        print("Name".ljust(35), " | ", "Id".ljust(5), " | ", "Author".ljust(20), " | ", "Quantity".ljust(8), " | ", "Category" )
+        print("----------------------------------------------------------------------------------------------------------------")
         for e in self.library:
-            print(e.name. ljust(35), " | ", e.id.ljust(5) ," | ", e.author.ljust(20)," | " , e.quantity)
+            print(e.name. ljust(35), " | ", e.id.ljust(5) ," | ", e.author.ljust(20)," | " , str(e.quantity).ljust(8), " | ", e.category)
         
                 
     def search_for_book(self):
@@ -77,30 +76,32 @@ class BookManangement:
 library=BookManangement()
 
 library.library = [
-    Book("Dế Mèn Phiêu Lưu Ký", "1", "Tô Hoài", 5),
-    Book("Tuổi Thơ Dữ Dội", "2", "Phùng Quán", 3),
-    Book("Lão Hạc", "3", "Nam Cao", 4),
-    Book("Chí Phèo", "4", "Nam Cao", 6),
-    Book("Tắt Đèn", "5", "Ngô Tất Tố", 2),
-    Book("Vợ Nhặt", "6", "Kim Lân", 3),
-    Book("Số Đỏ", "7", "Vũ Trọng Phụng", 5),
-    Book("Nhật Ký Trong Tù", "8", "Hồ Chí Minh", 4),
-    Book("Rừng Xà Nu", "9", "Nguyễn Trung Thành", 6),
-    Book("Đất Rừng Phương Nam", "10", "Đoàn Giỏi", 7),
-    Book("Mắt Biếc", "11", "Nguyễn Nhật Ánh", 8),
-    Book("Cho Tôi Xin Một Vé Đi Tuổi Thơ", "12", "Nguyễn Nhật Ánh", 6),
-    Book("Người Lái Đò Sông Đà", "13", "Nguyễn Tuân", 3),
-    Book("Chiếc Thuyền Ngoài Xa", "14", "Nguyễn Minh Châu", 4),
-    Book("Vợ Chồng A Phủ", "15", "Tô Hoài", 5),
-    Book("Hai Đứa Trẻ", "16", "Thạch Lam", 2),
-    Book("Cánh Đồng Bất Tận", "17", "Nguyễn Ngọc Tư", 4),
-    Book("Nỗi Buồn Chiến Tranh", "18", "Bảo Ninh", 3),
-    Book("Dòng Sông Ly Biệt", "19", "Nguyễn Mộng Giác", 2),
-    Book("Bến Không Chồng", "20", "Dương Hướng", 3)
+    Book("Dế Mèn Phiêu Lưu Ký", "1", "Tô Hoài", 5, "Thiếu nhi"),
+    Book("Tuổi Thơ Dữ Dội", "2", "Phùng Quán", 3, "Thiếu nhi"),
+    Book("Lão Hạc", "3", "Nam Cao", 4, "Văn học Việt Nam"),
+    Book("Chí Phèo", "4", "Nam Cao", 6, "Văn học Việt Nam"),
+    Book("Tắt Đèn", "5", "Ngô Tất Tố", 2, "Văn học Việt Nam"),
+    Book("Vợ Nhặt", "6", "Kim Lân", 3, "Văn học Việt Nam"),
+    Book("Số Đỏ", "7", "Vũ Trọng Phụng", 5, "Tiểu thuyết"),
+    Book("Nhật Ký Trong Tù", "8", "Hồ Chí Minh", 4, "Thơ"),
+    Book("Rừng Xà Nu", "9", "Nguyễn Trung Thành", 6, "Văn học Việt Nam"),
+    Book("Đất Rừng Phương Nam", "10", "Đoàn Giỏi", 7, "Thiếu nhi"),
+    Book("Mắt Biếc", "11", "Nguyễn Nhật Ánh", 8, "Tiểu thuyết"),
+    Book("Cho Tôi Xin Một Vé Đi Tuổi Thơ", "12", "Nguyễn Nhật Ánh", 6, "Thiếu nhi"),
+    Book("Người Lái Đò Sông Đà", "13", "Nguyễn Tuân", 3, "Tùy bút"),
+    Book("Chiếc Thuyền Ngoài Xa", "14", "Nguyễn Minh Châu", 4, "Truyện ngắn"),
+    Book("Vợ Chồng A Phủ", "15", "Tô Hoài", 5, "Văn học Việt Nam"),
+    Book("Hai Đứa Trẻ", "16", "Thạch Lam", 2, "Truyện ngắn"),
+    Book("Cánh Đồng Bất Tận", "17", "Nguyễn Ngọc Tư", 4, "Truyện ngắn"),
+    Book("Nỗi Buồn Chiến Tranh", "18", "Bảo Ninh", 3, "Tiểu thuyết"),
+    Book("Dòng Sông Ly Biệt", "19", "Nguyễn Mộng Giác", 2, "Tiểu thuyết"),
+    Book("Bến Không Chồng", "20", "Dương Hướng", 3, "Tiểu thuyết")
 ]
+
 
 library.display_booklist()
 
-
+while True:
+    print(library.find_book_by_id(input()).__str__())
 
 
